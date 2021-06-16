be92eab6b487f25de00457b9d5420801985bb111
laurence-lin <alksjdf12384@gmail.com>
2021-06-16 19:50:31 +0800

    update endpoint

--- main.py
+++ main.py
@@ -3,14 +3,19 @@ from flask import Flask
 import os
 
 import numpy as np
-import panda as pd
+import pandas as pd
 
 
 
-app = Flask("app")
+app = Flask(__name__)
 
 @app.route("/", methods = ['GET'])
 def main():
-    return "Welcome to the first APP!"
+    return "<head>Welcome to the first APP!</head>"
+
+
+@app.route("/test", methods = ['GET'])
+def test():
+   return "Test the endpoint!"
 
 
